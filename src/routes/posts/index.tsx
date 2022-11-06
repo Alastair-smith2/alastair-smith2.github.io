008b8fd892bc9b3766737580486590404877b0e6
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2022-11-06 15:37:53 +0000

    Title: Add post commit to automate deploy
    Body: Update new blog mdx as well as automate deploy step

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -1,4 +1,9 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
 import PostList from "~/components/post-list";
 
 export default component$(() => <PostList />);
+
+export const head: DocumentHead = {
+  title: "Posts",
+};
