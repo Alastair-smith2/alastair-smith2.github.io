b6ba831868483eca7f170a630ec23de87d2291c8
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2023-03-20 17:04:35 +0000

    Title: Update about

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -10,34 +10,37 @@ export default component$(() => {
       </h2>
       <Subheader
         text={
-          "Programming wise, some of the items I find most interesting are:"
+          "Programming-wise, some of the items I find most interesting are:"
         }
       />
       <ul class="mt-2 list-disc list-inside">
-        <li class="text-white">Rust</li>
+        <li class="text-white">Rust lang</li>
         <li class="text-white">
           Other "challenger" languages (e.g. Go / Kotlin / WebAssembly)
         </li>
         <li class="text-white">
           Social dynamics around software engineering (e.g. benefits of pair
-          programming / trunk based development to mention a couple)
+          programming and trunk based development to mention a couple)
         </li>
         <li class="text-white">Programming paradigms (FP / OO)</li>
         <li class="text-white">
-          How various technologies are advancing (e.g. hydration / resumability
-          / islands in the frontend world)
+          How various technologies are advancing (e.g. hydration, resumability
+          and islands in the frontend world)
         </li>
         <li class="text-white">Distributed systems</li>
         <li class="text-white">Security</li>
       </ul>
-      <Subheader text={"Non programming wise, things I enjoy:"} />
+      <Subheader text={"Non programming-wise, things I enjoy:"} />
       <ul class="mt-2 list-disc list-inside">
-        <li class="text-white">Games (board / video)</li>
-        <li class="text-white">Football (AKA soccer)</li>
-        <li class="text-white">Cooking</li>
-        <li class="text-white">Walking</li>
-        <li class="text-white">Animals</li>
-        <li class="text-white">Spending time with family / friends</li>
+        <li class="text-white">
+          Board games and video games (have loved Ori and the Will of the Wisps
+          recently)
+        </li>
+        <li class="text-white">Playing football (AKA soccer)</li>
+        <li class="text-white">Cooking delicious food from around the world</li>
+        <li class="text-white">Walking in the hills near where I live</li>
+        <li class="text-white">Animals (especially cats!)</li>
+        <li class="text-white">Spending time with family and friends</li>
       </ul>
       <p class="text-white text-lg mt-6">
         If you're interested in reaching out, either Twitter (at the moment,
