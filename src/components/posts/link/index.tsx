8e51d21e5ba08902a62c9ec838628681a18b6357
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2023-03-30 17:28:11 +0100

    Title: Fix screen dark / light mode preference issue
    Body: Add contents for statecharts as well

--- src/components/posts/link/index.tsx
+++ src/components/posts/link/index.tsx
@@ -9,14 +9,12 @@ export default component$(
         <Link
           href={props.post.href}
           class="
-          transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 block mt-6 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 block mt-6 p-6 max-w-sm rounded-lg border shadow-md  bg-gray-800 border-gray-700 hover:bg-gray-700"
         >
-          <h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h4 class="mb-2 text-2xl font-bold tracking-tight text-white">
             {content?.[0]}
           </h4>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            {content?.[1]}
-          </p>
+          <p class="font-normal text-gray-400">{content?.[1]}</p>
         </Link>
       </li>
     );
